c990054349318d6767a613b95fce92ad0559ee0e
eTiMaGo <etimago@gmail.com>
2021-02-12 03:05:16 +0700

    extras to register as messaging app

---
+++ app/src/main/java/net/bnfour/phone2web2tg_forwarder/ComposeSmsActivity.java
@@ -0,0 +1,6 @@
+package net.bnfour.phone2web2tg_forwarder;
+
+import android.app.Activity;
+
+public class ComposeSmsActivity extends Activity {
+}
---
+++ app/src/main/java/net/bnfour/phone2web2tg_forwarder/MmsReceiver.java
@@ -0,0 +1,12 @@
+package net.bnfour.phone2web2tg_forwarder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
