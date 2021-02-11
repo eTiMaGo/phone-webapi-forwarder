b33f1b82bc7817499ea7acbb9ea91b12a1bf3e73
eTiMaGo <etimago@gmail.com>
2021-02-11 21:12:52 +0700

    handle permissions

--- app/src/main/java/net/bnfour/phone2web2tg_forwarder/MainPreferencesActivity.java
+++ app/src/main/java/net/bnfour/phone2web2tg_forwarder/MainPreferencesActivity.java
@@ -1,16 +1,71 @@
 package net.bnfour.phone2web2tg_forwarder;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.TelephonyManager;
+
+
+import static android.Manifest.permission.READ_PHONE_NUMBERS;
+import static android.Manifest.permission.READ_PHONE_STATE;
+import static android.Manifest.permission.RECEIVE_SMS;
 
 public class MainPreferencesActivity extends Activity {
+
+    public static String mPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.edit().clear().apply();
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
+
+        if (ActivityCompat.checkSelfPermission(this, RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, READ_PHONE_NUMBERS) ==
+                        PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            TelephonyManager tMgr = (TelephonyManager)   this.getSystemService(Context.TELEPHONY_SERVICE);
+            mPhoneNumber = tMgr.getLine1Number();
+            //textView.setText(mPhoneNumber);
+            return;
+        } else {
+            requestPermission();
+        }
+
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        .replace(android.R.id.content, new SettingsFragment())
+        .commit();
     }
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{RECEIVE_SMS, READ_PHONE_NUMBERS, READ_PHONE_STATE}, 100);
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 100:
+                TelephonyManager tMgr = (TelephonyManager)  this.getSystemService(Context.TELEPHONY_SERVICE);
+                if (ActivityCompat.checkSelfPermission(this, RECEIVE_SMS) !=
+                        PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                        READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED  &&
+                        ActivityCompat.checkSelfPermission(this, READ_PHONE_STATE) !=      PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                mPhoneNumber = tMgr.getLine1Number();
+                //textView.setText(mPhoneNumber);
+                break;
+        }
+    }
+
+
 }
+
