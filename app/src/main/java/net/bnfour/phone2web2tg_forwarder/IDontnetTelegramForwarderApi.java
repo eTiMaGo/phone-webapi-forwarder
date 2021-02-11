4525ca2fdd4113b6a19c5eaf0d1b12dc6a75e7ef
eTiMaGo <etimago@gmail.com>
2021-02-11 21:13:00 +0700

    change endpoint

--- app/src/main/java/net/bnfour/phone2web2tg_forwarder/IDontnetTelegramForwarderApi.java
+++ app/src/main/java/net/bnfour/phone2web2tg_forwarder/IDontnetTelegramForwarderApi.java
@@ -8,6 +8,6 @@ import retrofit2.http.POST;
 public interface IDontnetTelegramForwarderApi {
     // so seems like retrofit doesn't like empty strings here
     // it also add slash between base url and request path by itself
-    @POST("sms_receive/")
+    @POST("smsnotify/")
     Call<Response> sendRequest(@Body Request request);
 }
